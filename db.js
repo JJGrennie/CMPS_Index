d25629c85162a9b0e61d8ad59a9848ee0bc44bd4
JJGrennie <jjgrenn@pointpark.edu>
2025-03-31 16:49:30 -0400

    Initial commit for dev branch

---
+++ db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+	user: "postgres",
+	password: "RossBoss",
+	host: "localhost",
+	database: "film",
+	port: 5432,	
+});
+
+module.exports = pool;
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const filmRoutes = require("./src/film/routes");
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors({
+	origin: '*'
+}));
+
+app.get("/", (req, res)=> {
+	res.send("Hello Point Park University");
+})
+
+//API Route
+app.use("/api/v1/film", filmRoutes);
+
+app.listen(port, () => console.log('running on ${port}'));
+
