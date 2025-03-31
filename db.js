a18b68e69800ef7bab22a08660975902e3b63dbd
Josh Grenninger <jjgrenn@pointpark.edu>
2025-03-31 17:19:26 -0400

    Update db.js
    Update to Render Host

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-	user: "postgres",
-	password: "RossBoss",
-	host: "localhost",
-	database: "film",
+	user: "jjgrennie",
+	password: "OWzW6D37ayVUa44UjRGmM9H2BCNQ5C8G",
+	host: "dpg-cvlg31ngi27c73fsa7a0",
+	database: "film_gwnh",
 	port: 5432,	
 });
 
