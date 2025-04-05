470ebc37508b17e8ae4d774b69cd46f506c0f23a
Josh Grenninger <jjgrenn@pointpark.edu>
2025-04-05 15:38:35 -0400

    Update db.js
    added ssl

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
 	password: "OWzW6D37ayVUa44UjRGmM9H2BCNQ5C8G",
 	host: "dpg-cvlg31ngi27c73fsa7a0-a.ohio-postgres.render.com",
 	database: "film_gwnh",
-	port: 5432,	
+	port: 5432,
+	ssl: true
 });
 
 module.exports = pool;
