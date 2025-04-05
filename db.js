05b8033c3944bbb2570b7a6ea215a2df136a8067
Josh Grenninger <jjgrenn@pointpark.edu>
2025-04-05 15:30:10 -0400

    Update db.js
    Update to Database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
 	user: "jjgrennie",
 	password: "OWzW6D37ayVUa44UjRGmM9H2BCNQ5C8G",
-	host: "dpg-cvlg31ngi27c73fsa7a0",
+	host: "dpg-cvlg31ngi27c73fsa7a0-a.ohio-postgres.render.com",
 	database: "film_gwnh",
 	port: 5432,	
 });
