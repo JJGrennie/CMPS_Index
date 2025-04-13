5b3a235ae4b008ec23c42780ba0cc2f54171da59
Josh Grenninger <jjgrenn@pointpark.edu>
2025-04-12 21:56:44 -0400

    Update routes.js
    Add router.get genre

--- src/film/routes.js
+++ src/film/routes.js
@@ -15,6 +15,9 @@ router.post('/', controller.addFilm);
 // Update an existing film by ID
 router.put('/:id', controller.updateFilm);
 
+// New route for fetching distinct genres
+router.get('/genre', controller.getGenres);  
+
 
 
 module.exports = router; 
